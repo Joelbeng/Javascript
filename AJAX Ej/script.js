d4c961921d31d32da7c35050dd02d4b67f460e94
joelbeng <joelbengolea@gmail.com>
2020-05-10 21:07:40 -0300

    pixel art fixed, draw and erase draggable on click

---
+++ AJAX Ej/script.js
@@ -0,0 +1,25 @@
+const imgTitle = document.getElementsByTagName('h2')[0];
+const img = document.getElementsByTagName('img')[0];
+const description = document.getElementsByClassName('description')[0];
+const dateImg = document.getElementsByClassName('date')[0];
+
+function getData(){
+    let xhr = new XMLHttpRequest();
+
+    xhr.addEventListener('load', function() {
+        if (this.status == 200) {
+           const NASAData = JSON.parse(this.responseText);
+           
+           imgTitle.textContent = NASAData.title;
+           img.alt =NASAData.title;
+           img.src = NASAData.hdurl;
+           dateImg.textContent = NASAData.date;
+           description.textContent = NASAData.explanation;   
+        } else {
+            imgTitle.textContet = 'El pedido fue fallido';
+        }
+    });
+
+    xhr.open('GET',"https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY");
+    xhr.send();
+}
--- pixel-art/script.js
+++ pixel-art/script.js
@@ -1,98 +1,112 @@
-window.addEventListener("load", function () {
+// Selección de elementos del DOM
+const btnReset = document.getElementById("reset");
+const btnBackColor = document.getElementById("backColorInput");
+const btnColor = document.getElementById("colorInput");
+const btnDraw = document.getElementById("pencil");
+const btnErase = document.getElementById("eraser");
 
+// flag que cambia de valor si clickeamos sobre el botón de dibujar o de borrar.
+let paint = true; 
+
+// Una vez que finalice de cargar el DOM, se ejecuta el siguiente bloque de código (la callback function).
+window.addEventListener("load", function () {
     const rowCount = document.getElementById("rowInput");
     const colCount = document.getElementById("columnInput");
     const cellSize = document.getElementById("input-size");
     const containerDiv = document.querySelector(".grid-container");
-    const btnReset = document.getElementById("reset");
-    const btnBackColor = document.getElementById("backColorInput");
-    const btnColor = document.getElementById("colorInput");
-    const btnDraw = document.getElementById("pencil");
-    const btnErase = document.getElementById("eraser");
-    let cellArray = containerDiv.children;
 
+    /** 
+    * Crea y muestra la grilla a partir de los valores ingresados en los inputs de filas y columnas.
+    */
     function showGrid() {
         containerDiv.style.gridTemplateColumns = `repeat(${colCount.value}, 1fr)`;
-        containerDiv.style.gridTemplateRows = `repeat(${rowCount.value}, 1fr)`;
         
+        // Crea un div por cada celda y lo asigna como hijo del contenedor
         for (let i = 0; i < rowCount.value * colCount.value; i++) {
             const newDiv = document.createElement("div");
             newDiv.classList.add("grid-item");
 
             newDiv.style.width = `${cellSize.value}px`;
             newDiv.style.height = `${cellSize.value}px`;
-            containerDiv.appendChild(newDiv);
             
-            // DUDA, ¿cada vez que clickeo para pintar o borrar se ejecuta toda la funcion showGrid? ¿Si es así es mejor hacer un for afuera?
-            //Evento que colorea si apretamos el lápiz.
-            btnDraw.addEventListener('click',function() {
-                if(getComputedStyle.opacity !== 1) this.style.opacity = "1";
-                btnErase.style.opacity = "0.5";
-                      
-                newDiv.addEventListener('click',function() {
-                    this.style.backgroundColor = btnColor.value;
-                });
-            });    
+            //Se le añade el atributo draggable con valor false, para que no tome al elemento newDiv como arrastrable. No funciona a la perfección
+            newDiv.setAttribute("draggable","false"); 
+            
+            //Evento que pinta o borra (pinta del color de fondo) dependiendo del valor del flag "paint", si presionamos el click izquierdo sobre una celda
+            newDiv.addEventListener("mousedown", function(ev){
+                if (paint) {
+                    ev.buttons == 1 ? this.style.backgroundColor = btnColor.value : null;
+                } else {
+                    ev.buttons == 1 ? this.style.backgroundColor = btnBackColor.value : null;
+                }
+            });
 
-            //Evento que "borra" (colorea con el color de fondo) si apretamos el borrador.
-            btnErase.addEventListener('click',function(){
-                if(getComputedStyle.opacity !== 1) this.style.opacity = "1";
-                btnDraw.style.opacity = "0.5";                            
-                
-                newDiv.addEventListener('mouseover',function() {
-                    this.style.backgroundColor = btnBackColor.value;
-                });
-                newDiv.addEventListener('click',function() {
-                    this.style.backgroundColor = "";
-                });
-                               
-            });        
+            //Evento que pinta o borra (pinta del color de fondo) dependiendo del valor del flag "paint", si arrastramos el cursor presionando el click izquierdo
+            newDiv.addEventListener("mouseenter", function(ev){
+                if (paint) {
+                    ev.buttons == 1 ? this.style.backgroundColor = btnColor.value : null;
+                } else {
+                    ev.buttons == 1 ? this.style.backgroundColor = btnBackColor.value : null;
+                }
+            });
+
+            containerDiv.appendChild(newDiv); 
         }
     }
 
     showGrid();
+    //Event Listeners
 
-    // Antes de volver a ejecutar la función showGrid, se elimina el contenido de containerDiv para que no se vayan sumando.
+    //Haciendo click en "Reiniciar" se borra la grilla (para que no se superpongan) y se ejecuta la función showGrid()
     btnReset.addEventListener('click', () => {
         containerDiv.innerHTML = '';
+        // if(!checkbox.checked) checkbox.checked;   --> Esta línea no funciona pero no entiendo por qué.
         showGrid();
     });
     
-    // Cada vez que se cambia el valor del input cellSize se modifica el tamaño de todos las celdas.
+    btnErase.addEventListener('click', function() {
+        paint = false;
+        
+        this.classList.remove("disabled");
+        btnDraw.classList.add("disabled");
+    });
+
+    btnDraw.addEventListener('click', function() {
+        paint = true;
+        
+        this.classList.remove("disabled");
+        btnErase.classList.add("disabled");
+    });
+    
+    let cells = containerDiv.children;
+
+    // Cada vez que se cambia el valor del input cellSize se modifica el tamaño de todas las celdas.
     cellSize.addEventListener('change', function() {
-        for(let i = 0; i < cellArray.length; i++) {
-            const cell = cellArray[i];
+        for(let i = 0; i < cells.length; i++) {
+            const cell = cells[i];
 
             cell.style.width = `${this.value}px`;
             cell.style.height = `${this.value}px`; 
         }                    
     });
 
-    //Cambia el fondo del contenedor de la grilla
+    //Cambia el color de fondo de la grilla
     btnBackColor.addEventListener('change', function() {
         containerDiv.style.backgroundColor = `${this.value}`;
     });
-
 
     const checkbox = document.getElementById("checkbox");
 
     //Muestra o no los bordes de las celdas
     checkbox.addEventListener('change', function() { 
-        for (let i = 0; i < cellArray.length; i++) {
-            cell = cellArray[i];
+        for (let i = 0; i < cells.length; i++) {
+            cell = cells[i];
 
-            if (this.checked !== true) {    
+            if (!this.checked) {    
                 cell.style.border = "none";
             } else {
                 cell.style.border ="1px solid black";
             }
         }    
-    });
-    
-    //Muestra un Alert en caso de que el usuario quiera pintar antes de seleccionar el lápiz
-    containerDiv.addEventListener('click', function(){
-        if (getComputedStyle(btnDraw).opacity == 1 && getComputedStyle(btnErase).opacity == 1){
-            alert("Haga click sobre el lápiz antes de comenzar")
-        }
     });
 });
